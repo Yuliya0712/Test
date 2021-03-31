317c6d506b56ae6ec514f9c710e98f374c4b4598
Yuliya <yulenka.rodkina@gmail.com>
2021-03-31 20:32:18 +0300

    add training17

---
+++ app/js/script.js
@@ -0,0 +1,55 @@
+// let array = [1, 'sum', 3];
+// let sum=0;
+
+// for (let i=array.length-1; i >=0; i--) {
+    // sum+=array[i];//sum=sum+array[i]
+    // console.log(array[i]);
+
+// }
+
+
+// let array = [1, 2, 3];
+// let arr= new Array(1,2,3);
+// let mas=[];
+// let alt = new Array(5);
+
+// let matrix=[
+//     [1,2,3,0],
+//     [4,5,6],
+//     [7,8,9],
+// ];
+// console.log(matrix[1][1]);
+// let sum=0;
+
+// for (let i=0;i<matrix.length; i++) {
+//    for (let j=0;j<matrix[i].length;j++)
+//    {
+//     console.log(matrix[i][j]);
+//     sum+=matrix[i][j];
+// }
+// }
+// console.log(sum);
+
+//  let users = [];
+//  for (let i = 0; 1; i++) {
+//    let str = prompt("Input your name", "");
+//    if (str == null) {
+//      console.log("cancel was pressed");
+//      break;
+//    } else {
+//      users.push(str);
+//    }
+//  }
+//  console.log(users);
+
+let str='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus non voluptatum dolorum, esse perspiciatis eaque?????';
+let words = str.split(' ');
+// words[7]
+for(let i=0; i<tmp.length;i++){
+    if (tmp[i]!=','&& tmp[i]!='.') {
+       console.log(tmp[i]); 
+    }
+
+}
+
+console.log(words);
