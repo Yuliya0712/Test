a74556a66f9dae7be22e82df141c6559611064e1
Yuliya <yulenka.rodkina@gmail.com>
2021-04-29 19:08:03 +0300

    add training25

--- app/js/script.js
+++ app/js/script.js
@@ -1,32 +1,51 @@
-// function showHelloWorld(){
-//     alert('Hello World 2');
-// }
-// btn.onclick = function(){
-//     alert('Hello World 3');
-// }
-// let evnt = "click";
-// btn.addEventListener("click", function(){alert('Hello World 3')});
+console.log('start');
+setTimeout(
+    function() {alert('stop');},5000
+)
+clearTimeout(id);
+console.log('finish');
+
+// $.ajax({
+//     type: 'GET',
+//     cache: false,
+//     url: 'https://jsonplaceholder.typicode.com/todos/',
+//     success: function(data, textStatus,request){
+//     // console.log(data);
+//     data = JSON.stringify(data);
+//     // console.log(data);
+//     data = JSON.parse(data);
+//     // console.log(typeof data);
+    
+// let result = "";
+//     for (let i = 0; i < data.length; i++) {
+//     //   console.log(data[i].userId);
+//     //   console.log(data[i].id);
+//     //   console.log(data[i].title);
+//     //   console.log(data[i].completed);
+
+//      result+= 
+//      '<tr>'+
+//      '<td>'+
+//      data[i].userId+
+//      '</td>'+
+//      '<td>'+
+//      data[i].id+
+//      '</td>'+
+//      '<td>'+
+//      data[i].title+
+//      '</td>'+
+//      '<td>'+
+//      data[i].completed;
+//      '</td>'+
+//      '</tr>'+
+//     }
+//     $('#result').append("<table>"+result+"</table>");
 
 
-// function showHelloWorld(){
-//     alert(this.innerHTML);
-//     this.innerHTML = "click me";
+//     // console.log(textStatus);
+//     // console.log(request);
+// },
+// error: function (error){
+//     console.log(error);
 // }
-// function showValue(){
-//     alert(btn.value);
-// }
-
-// let evnt = "click";
-// btn.addEventListener("click", function showText());
-// function showText(){
-//     console.log(text.value);
-// }
-
-function showValue(){
-//  alert(btn.value);
-if (event.altkey == true)
-console.log("Altkey was pressed");
-else{
-    console.log("Altkey wasn't pressed");
-}
-}
+// })
