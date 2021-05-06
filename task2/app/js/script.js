09d80447d94c1817d2f5cbd6a6db8fb4000dcded
Yuliya <yulenka.rodkina@gmail.com>
2021-05-06 19:04:09 +0300

    add training24

--- task2/app/js/script.js
+++ task2/app/js/script.js
@@ -1,12 +1,48 @@
-
-btn.setAttribute("value","checked");
-console.log(btn.value);
-console.log(btn.classList.toggle('text'));
-
-for (let i = 1; i <= 5; i++ ){
-let newLi = document.createElement('li');
-newLi.innerHTML = [i];
-list.appendChild(newLi);
+try{
+    console.log("show");
+    //  i;
+    throw new SyntaxError("возникла ошибка");
+     console.log("hidden");
+}catch(err){
+    console.log("Err + " + err.message + "---" + err.stack);
+}finally {
+    console.log("app is running");
 }
+console.log("script");
 
 
+
+
+
+
+
+// $("#box").on("click",function(){
+//     alert("Hello World");
+// })
+
+// $("#box").click(function() {
+//     $("#box").animate({left: "100px"})
+// })
+
+// $("#box").hover(function(){
+//     $("#box").css("background-color","grey");
+// }, function(){
+//     $("#box").css("background-color","brown");
+// } );
+
+// $(window).resize(function(){
+//     console.log("Resize event");
+// })
+
+// $("#box").scroll(function() {
+//     alert("Hello World");
+// })
+
+// $.ajax({
+//     type: 'GET',
+//     dataType: 'json',
+//     url: 'https://jsonplaceholder.typicode.com/posts/1',
+//     success: function(data) {
+//     $('#box').html(data);
+//     }
+//     });
